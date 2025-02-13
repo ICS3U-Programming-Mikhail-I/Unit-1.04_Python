a494cafbb0bab6dd2f379350405f88afe250c3fb
Mikhail Ibrahim <Mikhail.ibrahim@stu.ocsb.ca>
2025-02-13 18:52:15 +0000

    Added basic math operations program

---
+++ basic_math.py
@@ -0,0 +1,19 @@
+add_result = 12 + 8
+print("Addition: 12 + 8 =", add_result)
+
+sub_result = 20 - 7
+print("Subtraction: 20 - 7 =", sub_result)
+
+mul_result = 9 * 5
+print("Multiplication: 9 * 5 =", mul_result)
+
+div_result = 17 / 4
+print("Division: 17 / 4 =", div_result)
+
+power_result = 4**3
+print("Exponentiation: 4^3 =", power_result)
+
+import math
+
+sqrt_result = math.sqrt(25)
+print("Square Root: √25 =", sqrt_result)
